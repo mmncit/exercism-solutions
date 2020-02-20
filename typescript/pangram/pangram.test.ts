d222bbcd492447223971ed94d0a7524088901c76
mamun <mamun.rashid@seequent.com>
2020-02-20 16:34:07 -0700

    pangram solution

--- typescript/pangram/pangram.test.ts
+++ typescript/pangram/pangram.test.ts
@@ -1,4 +1,4 @@
-import Pangram from './pangram'
+import { createPangram } from './pangram'
 
 describe('Pangram()', () => {
   it('empty sentence', () => {
@@ -6,43 +6,43 @@ describe('Pangram()', () => {
     expect(pangram.isPangram()).toBe(false)
   })
 
-  xit('pangram with only lower case', () => {
-    const pangram = new Pangram("the quick brown fox jumps over the lazy dog")
+  it('pangram with only lower case', () => {
+    const pangram = createPangram("the quick brown fox jumps over the lazy dog")
     expect(pangram.isPangram()).toBe(true)
   })
 
-  xit("missing character 'x'", () => {
-    const pangram = new Pangram("a quick movement of the enemy will jeopardize five gunboats")
+  it("missing character 'x'", () => {
+    const pangram = createPangram("a quick movement of the enemy will jeopardize five gunboats")
     expect(pangram.isPangram()).toBe(false)
   })
 
-  xit("another missing character 'x'", () => {
-    const pangram = new Pangram("the quick brown fish jumps over the lazy dog")
+  it("another missing character 'x'", () => {
+    const pangram = createPangram("the quick brown fish jumps over the lazy dog")
     expect(pangram.isPangram()).toBe(false)
   })
 
-  xit("pangram with underscores", () => {
-    const pangram = new Pangram("the_quick_brown_fox_jumps_over_the_lazy_dog")
+  it("pangram with underscores", () => {
+    const pangram = createPangram("the_quick_brown_fox_jumps_over_the_lazy_dog")
     expect(pangram.isPangram()).toBe(true)
   })
 
-  xit("pangram with numbers", () => {
-    const pangram = new Pangram("the 1 quick brown fox jumps over the 2 lazy dogs")
+  it("pangram with numbers", () => {
+    const pangram = createPangram("the 1 quick brown fox jumps over the 2 lazy dogs")
     expect(pangram.isPangram()).toBe(true)
   })
 
-  xit('missing letters replaced by numbers', () => {
-    const pangram = new Pangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")
+  it('missing letters replaced by numbers', () => {
+    const pangram = createPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")
     expect(pangram.isPangram()).toBe(false)
   })
 
-  xit('pangram with mixed case and punctuation', () => {
-    const pangram = new Pangram("\"Five quacking Zephyrs jolt my wax bed.\"")
+  it('pangram with mixed case and punctuation', () => {
+    const pangram = createPangram("\"Five quacking Zephyrs jolt my wax bed.\"")
     expect(pangram.isPangram()).toBe(true)
   })
 
-  xit('pangram with non-ascii characters', () => {
-    const pangram = new Pangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.")
+  it('pangram with non-ascii characters', () => {
+    const pangram = createPangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.")
     expect(pangram.isPangram()).toBe(true)
   })
 })
--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,7 +1,33 @@
-function createPangram(s: string) {
+const isEmpty = (s: string) => s == ""
+const isLengthSmall = (s: string) => s.length < 26  // length of the sentence should be >= 26 to include all english letters
+
+const validatePangram = (s: string) => {
+    let indexOfLetters = new Array(26); 
+    indexOfLetters.fill(false);
+    let indexOfCurrentLetter: number = 0;
+    for(let i = 0; i < s.length; i++){
+        let ascii = s.charCodeAt(i);
+        if (65 <= ascii && ascii <= 90){
+            indexOfCurrentLetter = ascii - 65;
+        }
+        else if (97 <= ascii && ascii <= 122){
+            indexOfCurrentLetter = ascii - 97;
+        }
+        indexOfLetters[indexOfCurrentLetter] = true; 
+    }
+    for(let i = 0; i < indexOfLetters.length; i++){
+        if(indexOfLetters[i] == false){
+            return false;
+        }
+    }
+    return true;
+}
+
+export function createPangram(s: string) {
     return {
         isPangram() {
-          return false;
-      },
+            return ((isEmpty(s) || isLengthSmall(s)) ? false : validatePangram(s));
+        },
     }
-  }
+}
+
