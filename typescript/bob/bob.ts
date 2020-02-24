644ca14b3417ddb695fa7b4aa6729bbfe904590d
mamun <mamun.rashid@seequent.com>
2020-02-24 12:03:59 -0700

    bob solution

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,7 +1,21 @@
-class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+function createBob() {
+    return {
+        hey(s: string) {
+            s = s.trim();
+            if (s === ""){
+                return "Fine. Be that way!";
+            }
+            else if (s === s.toUpperCase() && s.match(/[a-z]/i) && s.charAt(s.length - 1) === "?") {
+                return "Calm down, I know what I\'m doing!";
+            }
+            else if(s === s.toUpperCase() && s.match(/[a-z]/i)){
+                return "Whoa, chill out!";
+            }
+            else if(s.charAt(s.length - 1) === "?"){
+                return "Sure.";
+            }
+            else return "Whatever.";
+        },
     }
 }
-
-export default Bob
+export default createBob;
