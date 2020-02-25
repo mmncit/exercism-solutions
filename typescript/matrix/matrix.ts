8d6eb8ff0aef4cc823f6af412d81adf1e3a80c2e
mamun <mamun.rashid@seequent.com>
2020-02-25 14:05:26 -0700

    solution of matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,7 +1,19 @@
-class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
+
+function createMatrix (s: string){   
+    return {
+        rows(n: number){
+            return s.split("\n")[n].split(" ").map(Number);
+        },
+        columns(n: number){
+            let nCol = this.rows(0).length;
+            let arr = s.split(/[\s, ]+/).map(Number);
+            let res = [];
+            for(let i = n; i < arr.length; i+=nCol){
+                res.push(arr[i]);
+            }
+            return res;
+        }
     }
 }
 
-export default Matrix
+export default createMatrix
